dc3c110f9c07abd81f0f6ee2823b9553a91e03d4
SarikaReddy <teegalasarika27@gmail.com>
2019-04-18 17:02:29 +0530

    Created collections

--- src/main/java/com/iiitb/SemWeb_NoSQL/DBConnection.java
+++ src/main/java/com/iiitb/SemWeb_NoSQL/DBConnection.java
@@ -1,9 +1,13 @@
 package com.iiitb.SemWeb_NoSQL;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;import java.util.Map;
 
 import org.bson.Document;
+import org.bson.conversions.Bson;
 
 import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
@@ -11,32 +15,45 @@ import com.mongodb.MongoClient;
 import com.mongodb.DBCollection;
 import com.mongodb.MongoException;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoIterable;
+import com.mongodb.client.model.Updates;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.Mongo;
+import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 
 public class DBConnection {
-	public static void main(String[] args) {
+	
+	static MongoClient mongoClient = null;
+	static MongoDatabase db = null;
+	
+	public static void establishConnection() {
+		try {
+
+			mongoClient = new MongoClient("localhost", 27017);
+			System.out.println("Connection build successfully");
+			
+		    db = mongoClient.getDatabase("university");
+		}
+		catch(MongoException e) {
+			
+			e.printStackTrace();
+		}
+	}
+	
+	public static void addClasses(ArrayList<String> classes) {
 
 		try
 		{
-			MongoClient mongoClient = new MongoClient("localhost", 27017);
-			System.out.println("Connection bulied successfully");
+			MongoCollection<Document> coll = db.getCollection("Class");
 			
-			MongoDatabase db=mongoClient.getDatabase("mongoDbDemo");
+			Document doc = null;
 			
-			MongoCollection<Document> table = db.getCollection("employee");  
-			//---------- Creating Document ---------------------------//    
-			Document doc = new Document("name", "Peter John");  
-			doc.append("id",12);  
-			Document doc2 =new Document("name","Parul Parikh");
-			doc2.append("id", 24);
-			//----------- Inserting Data ------------------------------//  
-			table.insertOne(doc);
-			table.insertOne(doc2);
-			
-			
-
+			for(String s : classes) {
+				doc = new Document("title",s);
+				coll.insertOne(doc);
+			}
 			
 		}
 		catch(MongoException e)
@@ -44,5 +61,111 @@ public class DBConnection {
 			e.printStackTrace();
 		}
 		
+	}
+
+	public static void addDataProperties(ArrayList<String> dproperties) {
+		try
+		{
+			MongoCollection<Document> coll = db.getCollection("DataTypeProperty");
+			
+			Document doc = null;
+			
+			for(String s : dproperties) {
+				doc = new Document("title",s);
+				coll.insertOne(doc);
+			}
+			
 		}
+		catch(MongoException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	public static void addObjectProperties(ArrayList<String> oproperties) {
+		try
+		{
+			MongoCollection<Document> coll = db.getCollection("ObjectProperty");
+			
+			Document doc = null;
+			
+			for(String s : oproperties) {
+				doc = new Document("title",s);
+				coll.insertOne(doc);
+			}
+			
+		}
+		catch(MongoException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	public static void addIndividuals(ArrayList<String> individuals) {
+		try
+		{
+			MongoCollection<Document> coll = db.getCollection("NamedIndividual");
+			
+			Document doc = null;
+			
+			for(String s : individuals) {
+				doc = new Document("title",s);
+				coll.insertOne(doc);
+			}
+			
+		}
+		catch(MongoException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	public static ArrayList<String> getDetails() {
+		
+		ArrayList<String> individuals = new ArrayList<>();
+		
+		try
+		{
+			MongoCollection<Document> coll = db.getCollection("NamedIndividual");
+			
+			FindIterable<Document> docs = coll.find();
+			
+			for(Document doc : docs) {
+				individuals.add(doc.getString("title"));
+			}
+			
+		}
+		catch(MongoException e)
+		{
+			e.printStackTrace();
+		}
+		return individuals;
+	}
+
+	public static void addInstanceDetails(List<HashMap<String, String>> list) {
+		
+		try
+		{
+			MongoCollection<Document> coll = db.getCollection("NamedIndividual");
+			
+			//FindIterable<Document> docs = coll.find();
+			
+			for(HashMap<String, String> map : list) {
+				
+				String temp = map.get("title");
+				FindIterable<Document> docs = coll.find();
+				Bson filter = new Document("title", temp);
+				for(Map.Entry<String,String> e : map.entrySet()) {
+					coll.updateOne(filter, {$set: {e.getKey():e.getValue()}});
+				}
+				
+				System.out.println("Here");
+			}
+			
+		}
+		catch(MongoException e)
+		{
+			e.printStackTrace();
+		}
+	}
 }
--- src/main/java/com/iiitb/SemWeb_NoSQL/Main.java
+++ src/main/java/com/iiitb/SemWeb_NoSQL/Main.java
@@ -1,16 +1,229 @@
 package com.iiitb.SemWeb_NoSQL;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryException;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ResultSet;
+import org.apache.jena.rdf.model.Literal;
 import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.util.FileManager;
 
-public class Main {
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.github.jsonldjava.core.JsonLdOptions;
+import com.github.jsonldjava.core.JsonLdProcessor;
+import com.github.jsonldjava.utils.JsonUtils;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-			
+public class Main {
+	
+	
+
+	public static void main(String[] args) throws FileNotFoundException {	
+		
 		FileManager.get().addLocatorClassLoader(Main.class.getClassLoader());
-		Model model = FileManager.get().loadModel("/home/sarika1/eclipse-workspace/SemWeb-NoSQL/src/main/resources/data.rdf");
-		model.write(System.out,"RDF/JSON");
+		Model model = FileManager.get().loadModel("/home/sarika1/Downloads/university.owl");
+		
+		DBConnection.establishConnection();
+		
+//		addClass(model); 
+//		addDataTypeProperty(model); 
+//		addObjectProperty(model);
+//		addIndividual(model);
+		
+		addDetails(model);
+	}
+	
+
+	static void addDetails(Model model) {
+		
+		List<HashMap<String, String>> list = new ArrayList<HashMap<String,String>>();
+		ArrayList<String> individuals = DBConnection.getDetails();
+		for(String s : individuals) {
+			
+			HashMap<String,String> map = new HashMap<String,String>();
+			map.put("title",s);
+			
+			String queryString = "prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" + 
+					"prefix owl: <http://www.w3.org/2002/07/owl#>\n" + 
+					"prefix ab: <http://www.semanticweb.org/sarika1/ontologies/2019/3/untitled-ontology-20#>\n" + 
+					"\n" + 
+					"SELECT ?predicate ?object\n" + 
+					"WHERE {\n" + 
+					"   ab:" + s +  "?predicate ?object\n" + 
+					"}";
+			Query query = QueryFactory.create(queryString);
+			QueryExecution qexec = QueryExecutionFactory.create(query, model);
+			try {
+				ResultSet results = qexec.execSelect();
+				while(results.hasNext()) {
+					QuerySolution soln = results.nextSolution();
+					RDFNode ynode = soln.get("object");
+					String predicate = null;
+					String object = null;
+			        if (ynode.isResource()) {
+			             predicate = soln.getResource("predicate").getURI();
+			             object = soln.getResource("object").getURI();
+			        } else {
+			        	 predicate = soln.getResource("predicate").getURI();
+			        	 object = soln.getLiteral("object").getString();
+			        }
+			        //System.out.println(predicate+" "+object);
+			        String[] words = predicate.split("#");
+			        String[] objects;
+			        if(object.contains("#")) {
+			        	objects = object.split("#");
+			        	if(!objects[1].equals("NamedIndividual"))
+			        		map.put(words[1],objects[1]);
+			        }
+			        else {
+			        	map.put(words[1],object);
+			        }
+				}
+			}
+			finally {
+				qexec.close();
+			}
+			list.add(map);
+			DBConnection.addInstanceDetails(list);
+			
+		}
+		
+		  for(Map<String,String> map : list){ 
+			  for(Map.Entry<String,String> e : map.entrySet()) { 
+				  System.out.println(e.getKey() + " " + e.getValue());
+			  }
+			  System.out.println("0");
+		  }
+		 
+		DBConnection.addInstanceDetails(list);
+	}
+
+
+	static void addClass(Model model) {
+		
+		  String queryString = "prefix owl: <http://www.w3.org/2002/07/owl#>\n" +
+		  "prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" + "\n" +
+		  "SELECT ?class WHERE { ?class a owl:Class }";
+		 
+		Query query = QueryFactory.create(queryString);
+		QueryExecution qexec = QueryExecutionFactory.create(query, model);
+		int i = 0;
+		ArrayList<String> classes = new ArrayList<>();
+		try {
+			ResultSet results = qexec.execSelect();
+			while(results.hasNext()) {
+				QuerySolution soln = results.nextSolution();
+				String name = soln.getResource("class").getURI();
+				String[] words = name.split("#");
+				classes.add(words[1]);
+			}
+		}
+		finally {
+			qexec.close();
+		}
+		DBConnection.addClasses(classes);
+	}
+	
+	static void addDataTypeProperty(Model model) {
+		
+		String queryString = "prefix owl: <http://www.w3.org/2002/07/owl#>\n" + 
+				"prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" + 
+				"\n" + 
+				"SELECT DISTINCT ?class \n" + 
+				"WHERE {\n" + 
+				"  ?class a owl:DatatypeProperty.\n" + 
+				"}";
+		Query query = QueryFactory.create(queryString);
+		QueryExecution qexec = QueryExecutionFactory.create(query, model);
+		int i = 0;
+		ArrayList<String> dproperties = new ArrayList<>();
+		try {
+			ResultSet results = qexec.execSelect();
+			while(results.hasNext()) {
+				QuerySolution soln = results.nextSolution();
+				String name = soln.getResource("class").getURI();
+				String[] words = name.split("#");
+				dproperties.add(words[1]);
+			}
+		}
+		finally {
+			qexec.close();
+		}
+		DBConnection.addDataProperties(dproperties);
+	
+	}
+
+	static void addObjectProperty(Model model) {
+		
+		String queryString = "prefix owl: <http://www.w3.org/2002/07/owl#>\n" + 
+				"prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" + 
+				"\n" + 
+				"SELECT DISTINCT ?class \n" + 
+				"WHERE {\n" + 
+				"  ?class a owl:ObjectProperty.\n" + 
+				"}";
+		Query query = QueryFactory.create(queryString);
+		QueryExecution qexec = QueryExecutionFactory.create(query, model);
+		int i = 0;
+		ArrayList<String> oproperties = new ArrayList<>();
+		try {
+			ResultSet results = qexec.execSelect();
+			while(results.hasNext()) {
+				QuerySolution soln = results.nextSolution();
+				String name = soln.getResource("class").getURI();
+				String[] words = name.split("#");
+				oproperties.add(words[1]);
+			}
+		}
+		finally {
+			qexec.close();
+		}
+		DBConnection.addObjectProperties(oproperties);
+
+	}
+
+	static void addIndividual(Model model) {
+		
+		String queryString = "prefix owl: <http://www.w3.org/2002/07/owl#>\n" + 
+				"prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" + 
+				"\n" + 
+				"SELECT DISTINCT ?class\n" + 
+				"WHERE {\n" + 
+				"  ?class a owl:NamedIndividual.\n" + 
+				"}";
+		Query query = QueryFactory.create(queryString);
+		QueryExecution qexec = QueryExecutionFactory.create(query, model);
+		int i = 0;
+		ArrayList<String> individuals = new ArrayList<>();
+		try {
+			ResultSet results = qexec.execSelect();
+			while(results.hasNext()) {
+				QuerySolution soln = results.nextSolution();
+				String name = soln.getResource("class").getURI();
+				String[] words = name.split("#");
+				individuals.add(words[1]);
+			}
+		}
+		finally {
+			qexec.close();
+		}
+		DBConnection.addIndividuals(individuals);
+
 	}
 
 }
